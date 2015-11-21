5cf482c297915202da6d8b55a0fc758a0ab726ed
huntzhan <programmer.zhx@gmail.com>
2015-11-21 17:49:56 +0800

    Init.

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/metadata.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+NAME = '{{cookiecutter.repo_name}}'
+VERSION = '{{cookiecutter.version}}'
+AUTHORS = [
+    '{{cookiecutter.full_name}}',
+]
+EMAILS = [
+    '{{cookiecutter.email}}',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = '{{cookiecutter.short_description}}'
