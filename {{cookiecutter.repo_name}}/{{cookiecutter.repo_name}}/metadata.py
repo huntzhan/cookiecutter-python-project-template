4c7563986e269b30a83f26acf053f30b11db6f80
huntzhan <programmer.zhx@gmail.com>
2016-04-17 19:04:10 +0800

    Bug fixed.

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/metadata.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/metadata.py
@@ -2,8 +2,6 @@
 from __future__ import (
     division, absolute_import, print_function, unicode_literals,
 )
-from builtins import *                  # noqa
-from future.builtins.disabled import *  # noqa
 
 
 NAME = '{{cookiecutter.repo_name}}'
